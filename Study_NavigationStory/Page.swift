3b894ce357ad5f891afe6785d6058605be85eb2c
Taeyoung Won <hexter1994@gmail.com>
2022-11-20 19:30:13 +0900

    Feat: Add Page 구조체

---
+++ Study_NavigationStory/Page.swift
@@ -0,0 +1,13 @@
+//
+//  Page.swift
+//  Study_NavigationStory
+//
+//  Created by 원태영 on 2022/11/20.
+//
+
+import Foundation
+
+struct Page {
+    let pageNumber : Int // 페이지 번호
+    let script : String // 페이지 내용
+}
