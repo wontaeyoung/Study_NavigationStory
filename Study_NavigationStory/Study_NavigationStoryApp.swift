f18114788412ebee6634dfdda6649b303d7f694c
Taeyoung Won <hexter1994@gmail.com>
2022-11-20 18:55:35 +0900

    Initial Commit

---
+++ Study_NavigationStory/Study_NavigationStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_NavigationStoryApp.swift
+//  Study_NavigationStory
+//
+//  Created by 원태영 on 2022/11/20.
+//
+
+import SwiftUI
+
+@main
+struct Study_NavigationStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
