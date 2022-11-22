399953567b268bc9097a5e349f9d240880f1909e
Taeyoung Won <hexter1994@gmail.com>
2022-11-22 22:32:00 +0900

    Feat: 단일 페이지 View 생성

---
+++ Study_NavigationStory/PageView.swift
@@ -0,0 +1,20 @@
+//
+//  PageView.swift
+//  Study_NavigationStory
+//
+//  Created by 원태영 on 2022/11/22.
+//
+
+import SwiftUI
+
+struct PageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView()
+    }
+}
