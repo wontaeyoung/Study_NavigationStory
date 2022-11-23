ca7f625e228577d1759c7597d0f20af19eb71543
Taeyoung Won <hexter1994@gmail.com>
2022-11-23 21:53:10 +0900

    Feat: Add Page struct

--- Study_NavigationStory/Page.swift
+++ Study_NavigationStory/Page.swift
@@ -7,7 +7,11 @@
 
 import Foundation
 
-struct Page {
-    let title : String // 페이지, 선택지 이름
-    var script : String { scripts[title]! }  // 페이지 내용
+struct Page : Hashable {
+    let id : Int
+    var title : String { pages[id]!.title} // 페이지, 선택지 이름
+    var script : String { pages[id]!.script }  // 페이지 내용
+    var nextPages: [Page] { pages[id]!.next } // 다음 페이지 리스트
 }
+
+
