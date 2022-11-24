a4384111bec1acafc252403f4475072ec27fded2
Taeyoung Won <hexter1994@gmail.com>
2022-11-24 09:17:02 +0900

    Refactor: Add attributes view modifier
    가독성을 위해 View에 적용된 속성자들을 ViewModifier 구조체로 분리

--- Study_NavigationStory/PageView.swift
+++ Study_NavigationStory/PageView.swift
@@ -14,39 +14,68 @@ struct PageView: View {
     
     var body: some View {
         VStack {
-
-            Spacer()
-            
-            Text(page.script)
-                .font(.title2)
-                .foregroundColor(Color("appleWhiteScript"))
-            
-            Spacer()
-            
-            ForEach(page.nextPages, id: \.self) {nextPage in
-                
-                NavigationLink(nextPage.title, destination: PageView(stack: $stack, page: nextPage))
-                    .fontWeight(.bold)
-                    .padding(18)
-                    .frame(width: 300)
-                    .foregroundColor(Color("appleWhite"))
-                    .background(Color("goldOrange"))
-                    .cornerRadius(20)
-                    .navigationTitle(page.title)
-                
+            if page.id == 7 {
+                AsyncImage(url: URL(string: "https://animalcrossing.soopoolleaf.com/image/acnlanimal/Timmy.png")!) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                } placeholder: {
+                    ProgressView()
+                }.frame(width: 250, height: 250)
             }
-            
-            
+            Spacer()
+            Text(page.script)
+                .modifier(ScriptModifier())
+            Spacer()
+            ForEach(page.nextPages, id: \.self) {nextPage in
+                NavigationLink(nextPage.title, destination: PageView(stack: $stack, page: nextPage))
+                    .modifier(NavigationModifier())
+                    .navigationTitle(page.title)
+            }
+            .padding(.bottom, 10)
+            if page.id == 9 {
+                Button {
+                    stack = .init()
+                } label: {
+                    Text("첫 화면으로 돌아가기")
+                        .modifier(NavigationModifier())
+                }
+
+            }
             Spacer()
 
         }
-        .padding(30)
-        .frame(width: 400)
-        .background(Color("darkBlue"))
-        
-        
+        .modifier(BackgroundModifier())
     }
-        
+}
+
+struct ScriptModifier : ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.title2)
+            .foregroundColor(Color("appleWhiteScript"))
+    }
+}
+
+struct BackgroundModifier : ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(30)
+            .frame(width: 400)
+            .background(Color("darkBlue"))
+    }
+}
+
+struct NavigationModifier : ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .fontWeight(.bold)
+            .padding(18)
+            .frame(width: 300)
+            .foregroundColor(Color("appleWhite"))
+            .background(Color("goldOrange"))
+            .cornerRadius(20)
+    }
 }
 
 //struct PageView_Previews: PreviewProvider {
